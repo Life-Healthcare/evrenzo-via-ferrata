4915e3fa40c33cefea958d3e1fe8da63dfb1de4a
Daniel <ppooii27@gmail.com>
2022-04-19 10:09:18 +0100

    fix dev3 nav size for ipad

--- dev3/client/src/components/nav/nav.tsx
+++ dev3/client/src/components/nav/nav.tsx
@@ -13,20 +13,20 @@ const Nav: React.FC<{ back?: number; forward?: string }> = (props) => {
       {props.back && (
         <ParallelogramButton>
           <button onClick={() => navigate(props.back)}>
-            <img src="./assets/nav-back-ico.png" alt="go back" />
+            <img src="./assets/nav-back-ico.png" alt="go back" className="icon1"/>
           </button>
         </ParallelogramButton>
       )}
       {props.forward && (
         <ParallelogramButton>
           <button onClick={() => navigate(props.forward)}>
-            <img src="./assets/nav-forward-ico.png" alt="go back" />
+            <img src="./assets/nav-forward-ico.png" alt="go back" className="icon2"/>
           </button>
         </ParallelogramButton>
       )}
       <ParallelogramButton>
-        <button onClick={() => navigate("/hlf")}>
-          <img src="./assets/nav-home-ico.png" alt="go back" />
+        <button onClick={() => navigate("/sustaining-life")}>
+          <img src="./assets/nav-home-ico.png" alt="go back" className="icon3"/>
         </button>
       </ParallelogramButton>
     </ButtonContainer>
