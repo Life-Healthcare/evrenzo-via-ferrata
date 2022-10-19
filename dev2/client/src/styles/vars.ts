f17d3575919462ba7f98f68eef589e9771a16d55
Y <y@Ys-MacBook-Pro.lan>
2022-10-19 14:00:23 +0100

    add: section 1

--- dev2/client/src/styles/vars.ts
+++
@@ -1,8 +0,0 @@
-const vars = {
-  rootSize: 20,
-  responsiveMin: 320,
-  responsiveMax: 1200,
-  fontBody: `"Trade Gothic LT Std Extended", Arial, sans-serif`,
-};
-
-export default vars;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,47 @@
+import ButtonStart from "../../components/ButtonStart"
+
+export default () => {
+    return (
+        <section 
+        style={{ backgroundImage: 'url(./images/section1-bg.png)'}}
+        className="bottom-mask relative flex flex-col w-full h-full bg-cover bg-fixed bg-center text-white p-24 pb-12 text-center">
+            <div className="flex">
+                <img 
+                className="w-[396px] h-[134px]"
+                src="./images/logo.svg" alt="logo" />
+                <div className="flex flex-grow justify-center mt-14">
+                    <div className="flex flex-col gap-7 w-[800px] translate-x-[calc(-396px/2)]">
+                        <p className="text-3xl">This information is intended for international audiences.</p>
+                        <p className="text-4xl">EVRENZO (roxadustat) IS NOT APPROVED FOR USE IN THE UNITED STATES</p>
+                        <p className="text-2xl">EVRENZO (roxadustat) has marketing authorisations in Japan, the European Union (EU) and the United Kingdom (UK),
+                        Turkey, Russia, South Africa, Egypt, UAE and Kuwait.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col flex-grow justify-center items-center gap-24">
+                <p className="text-7xl w-[800px]">Is the answer to anaemia of CKD in his blood?</p>
+                <ButtonStart/>
+            </div>
+            <div className="flex flex-col justify-end gap-10 h-[223px] text-2xl text-start font-semibold relative z-10">
+                <p className="w-[1153px]">
+                    This information is intended for international Healthcare Professionals and was created by Astellas Pharma Inc.
+                    This medication is subject to additional monitoring.
+                    EVRENZO™ is only available by prescription.
+                    Prescribing information may vary. Please refer to your local prescribing information.
+                </p>
+                <div className="flex justify-between">
+                    <p>
+                        MAT-NL-EVZ-2022-00020
+                        Date of preparation: October 2022
+                        Date of expiry: October 2022
+                    </p>
+                    <p className="w-[625px]">
+                    © October 2022 Astellas Pharma Inc. or its affiliates.
+                    All trademarks are the property of their respective owners.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
