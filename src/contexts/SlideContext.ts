db678eec082a75436f78e6fa1920f6700e1c6102
Y <y@Ys-MacBook-Pro.lan>
2022-10-19 16:09:31 +0100

    add: fix animations

--- src/contexts/SlideContext.ts
+++ src/contexts/SlideContext.ts
@@ -2,19 +2,19 @@ import { createContext } from "react";
 
 export const right = {
     initial: {
-        left: "100vw",
+        x: "100vw",
         transition: {
             duration: 0.4
         }
     },
     animate: {
-        left: "0",
+        x: "0",
         transition: {
             duration: 0.4
         }
     },
     exit: {
-        left: "-100vw",
+        x: "-100vw",
         transition: {
             duration: 0.4
         }
@@ -23,19 +23,19 @@ export const right = {
 
 export const left = {
     initial: {
-        left: "-100vw",
+        x: "-100vw",
         transition: {
             duration: 0.4
         }
     },
     animate: {
-        left: "0",
+        x: "0",
         transition: {
             duration: 0.4
         }
     },
     exit: {
-        left: "100vw",
+        x: "100vw",
         transition: {
             duration: 0.4
         }
