e87e01a6d3612dcdae6d2745c458a2efd8c9ffe6
Y <y@Ys-MacBook-Pro.lan>
2022-10-31 12:13:48 +0000

    fix screensaver text

--- src/pages/Sections/Section4/One.tsx
+++ src/pages/Sections/Section4/One.tsx
@@ -14,16 +14,16 @@ export default () => {
         variants={slideDirection} exit="exit" animate="animate" initial="initial"
         style={{ backgroundImage: `url(./images/section3-ss.png)`}}
         className="bottom-mask absolute flex flex-col w-full h-full bg-cover bg-fixed bg-center text-white p-24 pb-12 text-center">
-            <header className="flex text-gray-700">
+            <header className="flex">
                 <img 
                 className="w-[396px] h-[134px]"
                 src="./images/Logo2.svg" alt="logo" />
                 <div className="flex flex-grow justify-center mt-14">
                     <div className="flex flex-col gap-7 w-[800px] translate-x-[calc(-396px/2)]">
                         <p className="text-3xl">This information is intended for international audiences.</p>
-                        <p className="text-4xl">EVRENZO (roxadustat) IS NOT APPROVED FOR USE IN THE <br/> UNITED STATES</p>
+                        <p className="text-4xl">EVRENZO (roxadustat) IS NOT APPROVED FOR USE IN THE UNITED STATES</p>
                         <p className="text-2xl">EVRENZO (roxadustat) has marketing authorisations in Japan, <br/>the European Union (EU) and the United Kingdom (UK),
-                        Turkey, Russia, South Africa, Egypt, UAE and Kuwait.
+                        Turkey, Russia, <br/> South Africa, Egypt, UAE and Kuwait.
                         </p>
                     </div>
                 </div>
