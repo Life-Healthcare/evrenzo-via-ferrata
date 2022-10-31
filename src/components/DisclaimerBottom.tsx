af2523a52278d90d78fee15c3a85cc5b00d2dbe3
Y <y@Ys-MacBook-Pro.lan>
2022-10-31 14:59:34 +0000

    superscript trademark

--- src/components/DisclaimerBottom.tsx
+++ src/components/DisclaimerBottom.tsx
@@ -1,10 +1,11 @@
+import SmallText from "./SmallText"
 import Triangle from "./Triangle"
 
 export default () => {
     return (
         <div className="w-[1153px] flex flex-col gap-1">
             <p>This information is intended for international Healthcare Professionals and was created by Astellas Pharma Inc.</p>
-            <p><Triangle/> EVRENZOTM is subject to additional monitoring.</p>
+            <p><Triangle/> EVRENZO<SmallText>TM</SmallText> is subject to additional monitoring.</p>
             <p>For adverse events reporting, please contact: Pharmacovigilance.EST-C@astellas.com</p>
             <p>EVRENZO™ is only available by prescription.</p>
             <p>Prescribing information may vary. Please refer to your local prescribing information.</p>
--- src/pages/Sections/Sections.tsx
+++ src/pages/Sections/Sections.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom"
+import SmallText from "../../components/SmallText"
 import Triangle from "../../components/Triangle"
 import Section1 from "./Section1/Section1"
 import Section2 from "./Section2/Section2"
@@ -16,7 +17,7 @@ export default () => {
             {section === "4" && <Section4/>}
             {["1,3", "1,4", "2,2", "2,3", "3,2", "4,2", "4,4"]
             .some(p => section === p.split(",")[0] && page === p.split(",")[1]) && 
-            <p className="absolute bottom-0 left-2/4 -translate-x-2/4 text-2xl font-bold"><Triangle/> EVRENZOTM is subject to additional monitoring.</p>}
+            <p className="absolute bottom-0 left-2/4 -translate-x-2/4 text-2xl font-bold"><Triangle/> EVRENZO<SmallText>TM</SmallText> is subject to additional monitoring.</p>}
         </>
     )
 }
