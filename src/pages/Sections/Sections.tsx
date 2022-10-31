5334f3f503cc7864cef192d847b2d97545fd3e98
Y <y@Ys-MacBook-Pro.lan>
2022-10-31 14:53:40 +0000

    fix disclaimer text

--- src/pages/Sections/Sections.tsx
+++ src/pages/Sections/Sections.tsx
@@ -16,7 +16,7 @@ export default () => {
             {section === "4" && <Section4/>}
             {["1,3", "1,4", "2,2", "2,3", "3,2", "4,2", "4,4"]
             .some(p => section === p.split(",")[0] && page === p.split(",")[1]) && 
-            <p className="absolute bottom-0 left-2/4 -translate-x-2/4 text-2xl font-bold"><Triangle/> This medication is subject to additional monitoring.</p>}
+            <p className="absolute bottom-0 left-2/4 -translate-x-2/4 text-2xl font-bold"><Triangle/> EVRENZOTM is subject to additional monitoring.</p>}
         </>
     )
 }
