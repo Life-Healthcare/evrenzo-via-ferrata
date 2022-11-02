941825aa0a4ae2e981f1433b787b519087d87239
Y <y@Ys-MacBook-Pro.local>
2022-11-02 12:25:35 +0000

    refactoring

--- src/pages/Sections/Section1/Three.tsx
+++ src/pages/Sections/Section1/Three.tsx
@@ -13,40 +13,7 @@ export default () => {
         variants={slideDirection} exit="exit" animate="animate" initial="initial"
         style={{ backgroundImage: `url(./images/section1-bg.png)`}}
         className="absolute flex flex-col w-full h-full bg-cover bg-fixed bg-center text-gray-700 p-24 pb-12 text-center">
-            <header className="flex items-end justify-between mt-14 mb-20">
-                <h1 className="text-5xl text-primary">
-                    Living at high altitude
-                </h1>
-                <DisclaimerTop/>
-            </header>
-            <section className="grid grid-cols-2 text-[32px] text-start gap-10">
-                <div className="">
-                    High altitude is a challenging environment for most humans.<SmallText>1</SmallText> <br/> As elevation above sea level increases, the concentration of oxygen molecules decreases, causing hypoxia.
-                    <SmallText>1</SmallText>
-
-                    <p className="font-bold2 my-10">How oxygen varies with height<SmallText>2</SmallText></p>
-
-                    <img src="./images/section1-2-graph.png" alt="graph" />
-                </div>
-                <div className="flex flex-col gap-5">
-                    <p>
-                        Oxygen partial pressure reduces exponentially as height increases.<SmallText>3</SmallText>
-                    </p>
-                    <p>
-                    Oxygen partial pressure is only half its sea-level value at 5,000 m, the altitude of the Everest Base Camp, and only a third at the summit of Mount Everest.<SmallText>3</SmallText>
-                    </p>
-                    <p>
-                    Without acclimatisation, people who travel to these heights may suffer from altitude sickness.<SmallText>4</SmallText>
-                    </p>
-                    <p>
-                    Yet around the world, more than 81 million people live at high altitude (elevations above 2,500 m), and have adapted to the lower oxygen levels.<SmallText>5</SmallText>
-                    </p>
-                </div>
-            </section>
-            <footer className="flex flex-grow flex-col justify-end gap-10 h-[223px] text-2xl text-start font-semibold relative z-10">
-                <img className="absolute -bottom-12 -right-24"
-                src="./images/section1-2-img.png" alt="farmer" />
-            </footer>
+            <img src="" alt="" />
         </motion.article>
     )
 }
--- src/pages/Sections/Section2/One.tsx
+++ src/pages/Sections/Section2/One.tsx
@@ -13,26 +13,8 @@ export default () => {
         <motion.article 
         variants={slideDirection} exit="exit" animate="animate" initial="initial"
         style={{ backgroundImage: `url(./images/section2-ss.png)`}}
-        className="bottom-mask absolute flex flex-col w-full h-full bg-cover bg-fixed bg-center text-white p-24 pb-12 text-center">
-            <header className="flex">
-                <img 
-                className="w-[396px] h-[134px]"
-                src="./images/Logo2.svg" alt="logo" />
-                <div className="flex flex-grow justify-center mt-14">
-                    <div className="flex flex-col gap-7 w-[800px] translate-x-[calc(-396px/2)]">
-                        <p className="text-3xl">This information is intended for international audiences.</p>
-                        <p className="text-4xl">EVRENZO (roxadustat) IS NOT APPROVED FOR USE IN THE UNITED STATES</p>
-                        <p className="text-2xl">EVRENZO (roxadustat) has marketing authorisations in Japan, <br/>the European Union (EU) and the United Kingdom (UK),
-                        Turkey, Russia, <br/> South Africa, Egypt, UAE and Kuwait.
-                        </p>
-                    </div>
-                </div>
-            </header>
-            <section className="flex flex-col flex-grow justify-center items-center gap-24">
-                <h1 className="text-7xl w-[800px] text-gray-700">A Nobel Prize winning discovery</h1>
-                <ButtonStart onClick={() => navigate("/2/2")}/>
-            </section>
-            <ScreensaverFooter/>
+        className="bottom-mask absolute w-full h-full">
+            <img className="w-full h-full relative z-10" src="./pages/2.1.svg" alt="" />
         </motion.article>
     )
 }
