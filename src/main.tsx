bacc3251d0bcc19b56452baf11b435edd7f175b1
Daniel <ppooii27@gmail.com>
2022-11-02 09:46:13 +0000

    chore: pwa

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,19 @@ import { HashRouter as Router } from 'react-router-dom'
 import App from './routes'
 import './index.css'
 
+if ("serviceWorker" in navigator && process.env.NODE_ENV !== "development") {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
