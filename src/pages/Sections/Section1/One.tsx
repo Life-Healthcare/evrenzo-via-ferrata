adfe8d65d49cac33d10c0295fc05fd10cfc77351
Daniel <ppooii27@gmail.com>
2022-11-02 12:28:02 +0000

    chore: update start page & section1_1

--- src/pages/Sections/Section1/One.tsx
+++ src/pages/Sections/Section1/One.tsx
@@ -1,40 +1,32 @@
-import { useNavigate } from "react-router-dom"
-import ButtonStart from "../../../components/ButtonStart"
-import { motion } from "framer-motion"
-import { useContext } from "react"
-import SlideContext from "../../../contexts/SlideContext"
-import Triangle from "../../../components/Triangle"
-import DisclaimerBottom from "../../../components/DisclaimerBottom"
-import ScreensaverFooter from "../../../components/ScreensaverFooter"
+import { useNavigate } from "react-router-dom";
+import ButtonStart from "../../../components/ButtonStart";
+import { motion } from "framer-motion";
+import { useContext } from "react";
+import SlideContext from "../../../contexts/SlideContext";
+import Triangle from "../../../components/Triangle";
+import DisclaimerBottom from "../../../components/DisclaimerBottom";
+import ScreensaverFooter from "../../../components/ScreensaverFooter";
 
 export default () => {
-    const navigate = useNavigate();
-    const [slideDirection] = useContext(SlideContext);
+  const navigate = useNavigate();
+  const [slideDirection] = useContext(SlideContext);
 
-    return (
-        <motion.article 
-        variants={slideDirection} exit="exit" animate="animate" initial="initial"
-        style={{ backgroundImage: `url(./images/section1-ss.png)`}}
-        className="bottom-mask absolute flex flex-col w-full h-full bg-cover bg-fixed bg-center text-white p-24 pb-12 text-center">
-            <header className="flex">
-                <img 
-                className="w-[396px] h-[134px]"
-                src="./images/Logo.svg" alt="logo" />
-                <div className="flex flex-grow justify-center mt-14">
-                    <div className="flex flex-col gap-7 w-[800px] translate-x-[calc(-396px/2)]">
-                        <p className="text-3xl">This information is intended for international audiences.</p>
-                        <p className="text-4xl">EVRENZO (roxadustat) IS NOT APPROVED FOR USE IN THE UNITED STATES</p>
-                        <p className="text-2xl">EVRENZO (roxadustat) has marketing authorisations in Japan, <br/>the European Union (EU) and the United Kingdom (UK),
-                        Turkey, Russia, <br/> South Africa, Egypt, UAE and Kuwait.
-                        </p>
-                    </div>
-                </div>
-            </header>
-            <section className="flex flex-col flex-grow justify-center items-center gap-24">
-                <h1 className="text-7xl w-[1000px]">Is the answer to anaemia of CKD in his blood?</h1>
-                <ButtonStart onClick={() => navigate("/1/2")}/>
-            </section>
-            <ScreensaverFooter/>
-        </motion.article>
-    )
-}
+  return (
+    <motion.article
+      variants={slideDirection}
+      exit="exit"
+      animate="animate"
+      initial="initial"
+      style={{ backgroundImage: `url(./images/section1-ss.png)` }}
+      className="bottom-mask absolute flex flex-col w-full h-full bg-cover bg-fixed bg-center text-white p-12 text-center"
+    >
+      <img
+        className="w-[calc(1845px*var(--scale-x))] "
+        src="./images/section1-1.svg"
+      />
+      <section className="flex flex-col flex-grow justify-center items-center gap-24 absolute top-1/2 left-1/2 -translate-x-1/2">
+        <ButtonStart onClick={() => navigate("/1/2")} />
+      </section>
+    </motion.article>
+  );
+};
